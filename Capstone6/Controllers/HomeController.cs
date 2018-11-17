3c96ca06344de989702dfdcfe4d807baddcdd20d
Clayton <clayton.cox@gmail.com>
2018-11-17 15:42:33 -0500

    added view for user all list and added action for homecontroller

--- Capstone6/Controllers/HomeController.cs
+++ Capstone6/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Capstone6.Models; //added model to home controller cc
 
 namespace Capstone6.Controllers
 {
@@ -23,6 +24,25 @@ namespace Capstone6.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AllUserTasks(Task getTasks)
+        {
+
+            //1. get database
+            Capstone6Entities ormList = new Capstone6Entities();
+
+            //2.load items into viewbag matching userid
+            ViewBag.theUsersTasks = ormList.Tasks.Where(u => u.userID == getTasks.userID).ToString();
+
+
+            //3.return view
+            return View();
+        }
+        public ActionResult AllUserTasksView()
+        {
 
             return View();
         }
